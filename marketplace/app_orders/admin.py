85e5d82f7acd7d3b1771bbb254c6a72502779f9a
Valery <anokhina.lera.26@gmail.com>
2022-11-22 16:47:14 +0300

    created models for app_orders

--- marketplace/app_orders/admin.py
+++ marketplace/app_orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from app_orders.models import Delivery, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Delivery)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['type', 'price', 'min_order_cost']
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'cost', 'is_paid', 'delivery']
+    list_editable = ['is_paid']
+    list_filter = ['user', 'delivery']
+    inlines = [OrderItemInline]
