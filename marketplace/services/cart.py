79dae2ca83337bff3281055c36d1a0138a59f2a2
Valery <anokhina.lera.26@gmail.com>
2022-11-22 16:58:48 +0300

    added cart service

--- marketplace/services/cart.py
+++ marketplace/services/cart.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from app_shop.models import Product
 
 
 class Cart(object):
@@ -9,6 +10,19 @@ class Cart(object):
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def __iter__(self):
+        """ Перебор элементов в корзине и получение товаров из базы данных """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+        for item in self.cart.values():
+            yield item
+
+    def __len__(self):
+        """ Подсчет всех товаров в корзине """
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1):
         """ Добавление товара в корзину или обновление его количества """
@@ -31,6 +45,11 @@ class Cart(object):
         """ Сохранение корзины """
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
+
+    def get_total_price(self):
+        """ Подсчет итоговой стоимости товаров в корзине """
+        total_price = sum(item['price'] * item['quantity'] for item in self.cart.values())
+        return total_price
 
     def clear(self):
         """ Удаление корзины из сессии """
